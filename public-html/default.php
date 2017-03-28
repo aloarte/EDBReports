9937b4e728c64e8fec11998cff12d3ed0f572204
aloarte <aloarter@gmail.com>
2017-03-28 12:31:32 +0200

    Ordenado por el mismo día. Removidos enlaces en botones.

--- public-html/default.php
+++ public-html/default.php
@@ -1,10 +1,11 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <!-- Se define el documento como una app de angular con ng-app para referenciarlo en el app.js-->
-<html lang="es" ng-app="raidStuff">
+<html lang="es" ng-app="raidStuff" >
 	<head>
 		<title>Raid</title>
 		<!-- Se incluye bootstrap para darle estilo, angular y app.js donde encontrará todas las referencias-->
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="app.css" media="screen" />
 		<script type="text/javascript" src="angular.min.js"></script>
 		<script type="text/javascript" src="app.js"></script>
 	</head>
@@ -24,9 +25,9 @@
 				//Para cada boss
 				foreach(glob('./*', GLOB_ONLYDIR) as $dir) 
 				{
-					echo "<div class='list-group-item container-fluid' ng-show='store.jefes[$cntBoss].showBoss'>";
+					echo "<div  class='list-group-item container-fluid' ng-show='store.jefes[$cntBoss].showBoss'>";
 
-					echo "<div class='col-md-4'><h3>{{store.jefes[$cntBoss].name}}</h3>";
+					echo "<div class='col-md-4'><h2 class='lead'><b>{{store.jefes[$cntBoss].name}}</b></h2>";
 
 					echo "<div>
 							<div class='img-wrap'>
@@ -45,7 +46,7 @@
 					 * Cada boton selector tiene un ng-click que cambia el tab y asigna el nuevo valor al controlador.
 					 * Más abajo se mostrará un div u otro en función del valor del tab.
 					 */
-					echo "	<section class='tab' ng-controller='TabController as tab'>	
+					echo "	<section 'class='tab' ng-controller='TabController as tab'>	
 									<ul class='nav nav-pills list-inline'>
 										<li ng-class='{ active: tab.isSet(0) }'>
 											<a href ng-click='tab.setTab(0)'>Grupo 1</a>
@@ -54,7 +55,7 @@
 											<a href ng-click='tab.setTab(1)'>Grupo 2</a>
 										</li>
 									</ul>
-									<div class='row'>";
+									";
 
 
 					//Se obtiene cada grupo dentro de cada boss
@@ -66,31 +67,92 @@
 						/*
 						 * Muestra el div del grupo si este se seleccíonó previamente.
 						 */
-						echo "<div ng-show='tab.isSet($cntGroup)'>
-								<h5>".str_replace($dir."/", '', $group)."</h5>\n";
+						echo "<div class='container-fluid testimonial-group' ng-show='tab.isSet($cntGroup)'>";
+						echo "<div class='row text-left' >";
 
 
+						$files = scandir($group);	
+						$primero = true;
+						$fechaTransformadaAnterior="";
 
-						$files = scandir($group);						
-						foreach ($files as &$value) 
+						foreach ($files as &$refToFile) 
 							{
-								if(!in_array($value, $bosses))
+								if(!in_array($refToFile, $bosses))
 								{
-								echo "<p><a href='./".$group."/".$value."'>".$value."</a></p>\n";
+									//Se parte la cadena por el '-'
+									$refFirstSplit = explode('-',$refToFile);								
+									//Se saca la hora dividiendo el string sobrante de la anterior operación por '_' 
+									$refSecondSplit = explode('_',$refFirstSplit[1]);
+									//Se obtiene la fecha
+									$fechaReporte = $refFirstSplit[0];
+									$horaReporte = $refSecondSplit[0];
+
+									$anio = substr($fechaReporte,0,4); 
+									$mes = substr($fechaReporte,4,2); 
+									$dia = substr($fechaReporte,6,2); 
+
+									$hora = substr($horaReporte,0,2); 
+									$minuto = substr($horaReporte,2,2); 
+									$segundo = substr($horaReporte,4,2); 
+
+									$fechaTransformada = "".$dia."/".$mes."/".$anio;
+									$horaTransformada = "".$hora.":".$minuto.":".$segundo;
+
+									if($primero){
+										//echo "1 $fechaTransformada - $fechaTransformadaAnterior";
+										//guardo la fecha anterior
+										$fechaTransformadaAnterior= $fechaTransformada;
+										$primero = false;
+										echo "<div class='col-xs-4'>";
+										echo "<p> <b> Dia: $fechaTransformada</b></p>";
+										echo "<p> Hora: $horaTransformada</p>";
+										echo "<p><a class='btn btn-primary' href='./".$group."/".$refToFile."'>Ver Reporte</a></p>\n";
+									}
+									else if(strcmp($fechaTransformada,$fechaTransformadaAnterior)===0){
+										//echo "2: $fechaTransformada - $fechaTransformadaAnterior";
+										echo "<p> Hora: $horaTransformada</p>";
+										echo "<p><a class='btn btn-primary' href='./".$group."/".$refToFile."'>Ver Reporte</a></p>\n";
+
+									}
+									else if(strcmp($fechaTransformada,$fechaTransformadaAnterior)!==0){
+										//echo "3 $fechaTransformada - $fechaTransformadaAnterior";
+										//Se actualiza la nueva fecha anterior
+										$fechaTransformadaAnterior= $fechaTransformada;
+										echo "</div>";
+										echo "<div class='col-xs-4'>";
+										echo "<p> <b> Dia: $fechaTransformada</b></p>";
+										echo "<p> Hora: $horaTransformada</p>";
+										echo "<p><a class='btn btn-primary' href='./".$group."/".$refToFile."'>Ver Reporte</a></p>\n";
+							
+									}
+
+									
+
+									
 								}
 							}
-						echo "</div>\n";
+						//Tras juntar los reportes de un mismo día, si se realizó cierro esta cadena de divs. En caso contrario no	
+						if(!empty($fechaTransformadaAnterior)){
+							$fechaTransformadaAnterior="";
+							echo "</div>"; //Cierra el ultimo div de los reportes de un mismo dia
+						}
+						echo "</div>"; //cierra el div de row
+
+
+						echo "</div>"; //Cierra el div de cada grupo
+
 						$cntGroup++;
+
 					}
 
-					echo "</div>";
-					echo "</section>";
-					echo "</div>";
-					echo "</div>\n";
+					echo "</section>"; //Cierra la seccion
+					echo "</div>"; //cierra el div que recoge la seccion
+					echo "</div>\n"; //cierra el div que recoge el boss
 					$cntBoss ++;
 
 				}
 			 ?>
 		</div>
 	</body>
+
 </html>
